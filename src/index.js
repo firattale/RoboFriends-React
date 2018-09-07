7c071ba8be9659182b7124824f998003b4993a70
Firat Tale <talefirat@gmail.com>
2018-09-07 11:41:38 +0300

    add async redux

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import {createLogger} from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import {searchRobots} from './reducers'
+import { searchRobots, requestRobots } from './reducers'
 import 'tachyons';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
-	</Provider>,
-	 document.getElementById('root'));
+	</Provider>,document.getElementById('root'));
+
 registerServiceWorker();
