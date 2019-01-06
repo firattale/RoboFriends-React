13f6341cf893f04d82526349ee23ab300d0dc026
Firat Tale <talefirat@gmail.com>
2019-01-06 17:33:38 +0300

    Updates

---
+++ precache-manifest.e686cb36170694933f444fb610a1004d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cb4b3b188fa4552f6e1",
+    "url": "/RoboFriends-React/static/css/main.2fc0e5d5.chunk.css"
+  },
+  {
+    "revision": "9cb4b3b188fa4552f6e1",
+    "url": "/RoboFriends-React/static/js/main.9cb4b3b1.chunk.js"
+  },
+  {
+    "revision": "21bb93878664e87bb09b",
+    "url": "/RoboFriends-React/static/css/1.526dfedd.chunk.css"
+  },
+  {
+    "revision": "21bb93878664e87bb09b",
+    "url": "/RoboFriends-React/static/js/1.21bb9387.chunk.js"
+  },
+  {
+    "revision": "99745f397a3076c149ff",
+    "url": "/RoboFriends-React/static/js/runtime~main.99745f39.js"
+  },
+  {
+    "revision": "5600967edd3f1a027665ba2572f23016",
+    "url": "/RoboFriends-React/static/media/SEGA.5600967e.woff"
+  },
+  {
+    "revision": "0772910bb551bea802b3b6e53d98e220",
+    "url": "/RoboFriends-React/index.html"
+  }
+];
