e7b27b6b65a549465f8d5ebf10638d81608e6c2b
Firat Tale <talefirat@gmail.com>
2019-01-06 18:49:00 +0300

    made 100 points at lighthouse

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className="pa2">
       <input
+        aria-label="Search robots"
         type="search"
         className="pa3 ba b--green bg-lightest-blue"
         placeholder="search robots"
